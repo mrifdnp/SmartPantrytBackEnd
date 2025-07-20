bbb15f10ce9297f9a1fe2411077515286f2a50d1
Rifqi Dani Putranto <rifqidani23@gmail.com>
2025-07-20 16:38:35 +0700

    notif

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,19 @@
 self.addEventListener("push", function (event) {
-  const data = event.data?.json();
+  let data = {};
+  try {
+    data = event.data?.json() || {};
+  } catch (e) {
+    console.error("Failed to parse push event data", e);
+  }
 
-  const notificationData = data?.web?.notification || {};
+  const notificationData = data.notification || {};
 
   const title = notificationData.title || "Untitled";
   const options = {
     body: notificationData.body || "No body",
     data: {
       url: notificationData.deep_link || "https://smart-pantry-back-end.vercel.app/",
-    }
+    },
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
